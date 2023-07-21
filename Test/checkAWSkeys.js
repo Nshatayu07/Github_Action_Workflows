c4ce8f766db2d967d88cbb7966e043cb3cfaeeb9
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-07-21 14:43:48 +0530

    Update checkAWSkeys.js

--- Test/checkAWSkeys.js
+++ Test/checkAWSkeys.js
@@ -2,7 +2,4 @@
     func1.displayName="someName"
     console.log(func1.displayName)
 function f(){}
-// test_var =  "AKIAIOSFODNN7EXAMPLE"
-// target_id = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-
 console.log(test_var);
